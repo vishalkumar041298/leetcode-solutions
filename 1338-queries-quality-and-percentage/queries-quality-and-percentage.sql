1cea895806dea0b090d2a4ae23622bfaeeb2df61
Vishal Kumar <50990822+vishalkumar041298@users.noreply.github.com>
2025-02-25 00:06:34 +0000

    Time: 343 ms (98.26%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,8 @@
-select q.query_name, round(avg(rating/position), 2) as quality, round((
-select count(*) from Queries q2 where q2.rating < 3 and q.query_name=q2.query_name
- ) * 100/count(q.query_name), 2)   as poor_query_percentage from Queries q group by query_name
-
+select
+query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(rating < 3) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+group by
+query_name;
