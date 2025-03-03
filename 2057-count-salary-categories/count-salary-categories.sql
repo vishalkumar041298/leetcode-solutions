c9c1985e7c9ee7f86bbc1f869196990596dceedd
Vishal Kumar <50990822+vishalkumar041298@users.noreply.github.com>
2025-03-03 01:40:18 +0000

    Time: 3566 ms (15.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,5 @@
+select 'Low Salary' as  category, count(*) as accounts_count from accounts where income < 20000
+union all
+select 'Average Salary' as  category, count(*) as accounts_count from accounts where income between 20000 and 50000
+union all
+select 'High Salary' as  category, count(*) as accounts_count from accounts where income > 50000
